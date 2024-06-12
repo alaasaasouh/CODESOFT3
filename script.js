d2295f69db12178e6e9edb6aec6e9f7bd11f8230
alaasaasouh <134618977+alaasaasouh@users.noreply.github.com>
2024-06-12 20:49:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+const display = document.getElementById('display');
+const buttons = Array.from(document.getElementsByClassName('btn'));
+
+buttons.forEach(button => {
+    button.addEventListener('click', () => {
+        const value = button.getAttribute('data-value');
+        if (value === 'C') {
+            display.innerText = '';
+        } else if (value === '=') {
+            try {
+                display.innerText = eval(display.innerText);
+            } catch {
+                display.innerText = 'Error';
+            }
+        } else {
+            display.innerText += value;
+        }
+    });
+});
